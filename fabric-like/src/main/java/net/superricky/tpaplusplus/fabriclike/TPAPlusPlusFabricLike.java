c898490f97c2988d4696546ba0ef57a698fc1a62
SuperRicky14 <112232891+SuperRicky14@users.noreply.github.com>
2025-12-25 05:29:45 +0800

    Rewrite build toolchain based on latest architectury template (#18)
    Rewrite build toolchain and mod metadata based on latest architectury template

--- fabric-like/src/main/java/net/superricky/tpaplusplus/fabriclike/TPAPlusPlusFabricLike.java
+++
@@ -1,9 +0,0 @@
-package net.superricky.tpaplusplus.fabriclike;
-
-import net.superricky.tpaplusplus.TPAPlusPlus;
-
-public class TPAPlusPlusFabricLike {
-    public static void init() {
-        TPAPlusPlus.init();
-    }
-}

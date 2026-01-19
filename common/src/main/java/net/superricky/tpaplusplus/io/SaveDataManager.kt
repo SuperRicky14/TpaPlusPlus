0b2805d8bb9d18d7799d097eec446ff14db481e7
SuperRicky <112232891+SuperRicky14@users.noreply.github.com>
2026-01-19 18:33:53 +0800

    Add extra error handling for JsonSyntaxException, and switch to a separate lock object for saveData, since the previous one doesn't work for swapping.

--- common/src/main/java/net/superricky/tpaplusplus/io/SaveDataManager.kt
+++ common/src/main/java/net/superricky/tpaplusplus/io/SaveDataManager.kt
@@ -1,6 +1,7 @@
 package net.superricky.tpaplusplus.io
 
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
 import com.mojang.logging.LogUtils
 import net.minecraft.server.level.ServerPlayer
@@ -11,16 +12,19 @@ import java.io.FileWriter
 import java.io.IOException
 import java.util.*
 
+private val MOD_SAVEDATA_FILE_NAME = "tpaplusplus_savedata.json"
 private val MOD_SAVEDATA_FOLDER_PATH = "mods" + File.separator + ".tpaplusplus" + File.separator
-private val MOD_SAVEDATA_FILE_PATH = MOD_SAVEDATA_FOLDER_PATH + File.separator + "tpaplusplus_savedata.json"
+private val MOD_SAVEDATA_FILE_PATH = MOD_SAVEDATA_FOLDER_PATH + File.separator + MOD_SAVEDATA_FILE_NAME
 private val MOD_SAVEDATA_FOLDER = File(MOD_SAVEDATA_FOLDER_PATH)
 
 object SaveDataManager {
     private val LOGGER: Logger = LogUtils.getLogger()
 
+    private val saveDataLock = Any() // We use a separate lock object because we reassign saveData
+
     private var saveData: MutableMap<UUID, PlayerData> = hashMapOf()
 
-    fun getPlayerData(player: ServerPlayer): PlayerData = synchronized (saveData) {
+    fun getPlayerData(player: ServerPlayer): PlayerData = synchronized (saveDataLock) {
         return saveData.getOrPut(player.uuid) {
             PlayerData()
         }
@@ -38,7 +42,7 @@ object SaveDataManager {
         }
 
         try {
-            synchronized (saveData) {
+            synchronized (saveDataLock) {
                 FileWriter(MOD_SAVEDATA_FILE_PATH).use { writer ->
                     gson.toJson(saveData, writer)
                 }
@@ -49,20 +53,22 @@ object SaveDataManager {
         }
     }
 
-    fun loadPlayerData() = synchronized (saveData) {
+    fun loadPlayerData() = synchronized (saveDataLock) {
         val gson = GsonBuilder().setPrettyPrinting().create()
 
         if (!MOD_SAVEDATA_FOLDER.exists()) return
 
         try {
-            synchronized (saveData) {
-                FileReader(MOD_SAVEDATA_FILE_PATH).use { reader ->
-                    saveData = gson.fromJson(reader, object : TypeToken<MutableMap<UUID, PlayerData>>() {}.type)
-                    LOGGER.info("Successfully loaded player data!")
-                }
+            FileReader(MOD_SAVEDATA_FILE_PATH).use { reader ->
+                saveData = gson.fromJson(reader, object : TypeToken<MutableMap<UUID, PlayerData>>() {}.type)
+                LOGGER.info("Successfully loaded player data!")
             }
         } catch (e: IOException) {
             LOGGER.error("An IOException occurred when trying to load playerData.")
+            LOGGER.error(e.message)
+        } catch (e: JsonSyntaxException) {
+            LOGGER.error("An JsonSyntaxException occurred when trying to load playerData.")
+            LOGGER.error("Did you manually edit \"$MOD_SAVEDATA_FILE_NAME\"? If so, you might want to check your syntax!")
             LOGGER.error(e.message)
         }
     }

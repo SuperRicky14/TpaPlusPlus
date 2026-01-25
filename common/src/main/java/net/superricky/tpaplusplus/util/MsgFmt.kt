334ef0d3c9d4ef1993813077835892c55d4b4ec7
SuperRicky <112232891+SuperRicky14@users.noreply.github.com>
2026-01-25 15:46:17 +0800

    Makes the templating function work for any object. This is important for TPAPlusPlusCommand.java's functionality, where it returns a Supplier<String> to the user, which needs to be ran on every match.

--- common/src/main/java/net/superricky/tpaplusplus/util/MsgFmt.kt
+++ common/src/main/java/net/superricky/tpaplusplus/util/MsgFmt.kt
@@ -1,5 +1,6 @@
 package net.superricky.tpaplusplus.util
 
+import java.util.function.Supplier
 import java.util.regex.Matcher
 
 private val TEMPLATE_PATTERN = """(?<!\\)\$\{(\w+)}""".toRegex().toPattern()
@@ -9,16 +10,24 @@ private val TEMPLATE_PATTERN = """(?<!\\)\$\{(\w+)}""".toRegex().toPattern()
  * Useful for user-inputted strings.
  * @param replacements A map of placeholders (for example "name"), and their replacement (for example "John").
  * Placeholders only contain the name of the placeholder, not the ${} surrounding it.
- * Replacements should be human-readable.
+ * Replacements should be human-readable. If replacement is a Supplier or a zero-function lambda, it will be automatically unwrapped here into a string.
+ * The returned object of the Supplier/Lambda will have it's toString() method called.
+ * For anything that isn't a String, Supplier, or Lambda, it's toString() method will be called.
  * @return The templated string.
  */
-fun String.template(replacements: Map<String, String>): String {
+fun String.template(replacements: Map<String, Any>): String {
     val sb = StringBuilder()
     val matcher = TEMPLATE_PATTERN.matcher(this)
 
     while (matcher.find()) {
         val placeholder = matcher.group(1)
-        val replacement = replacements[placeholder] ?: matcher.group()
+        val replacement = when (val dynamicReplacement = replacements[placeholder]) {
+            is String -> dynamicReplacement
+            is Supplier<*> -> dynamicReplacement.get().toString()
+            is Function0<*> -> dynamicReplacement.invoke().toString()
+            is Nothing? -> matcher.group()
+            else -> dynamicReplacement.toString()
+        }
 
         matcher.appendReplacement(sb, Matcher.quoteReplacement(replacement))
     }
